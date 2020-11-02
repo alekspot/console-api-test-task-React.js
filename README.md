7fe9be58006cc3726381e18a4598ae244cab84eb
Aleksandr Potkin <apotkin@xdsoft.ru>
2020-11-02 18:46:47 +0300

    [+] link to github

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Тестовое задание: https://www.notion.so/Frontend-API-75cc5ecc28cd42f4a6f963e2dad88680
+
 Макет: https://www.figma.com/file/dCM3HsX0D9gD84angpbUB3oP/Test-exercise?node-id=8528%3A224
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,6 +6,7 @@ export const Footer = ({onSubmit, onFormat}) => {
     return (
         <div className="footer">
             <button className="submit" onClick={onSubmit}>Отправить</button>
+            <a href="https://github.com/alekspot/console-api-test-task-React.js">https://github.com/alekspot/console-api-test-task-React.js</a>
             <button className="btn footer__format" onClick={onFormat}>
                 <span className="footer__format-icon"><IconFormat/></span>
                 <span>Форматировать</span>
